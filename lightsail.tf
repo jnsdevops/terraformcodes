2055f0394521b214eeadd9f3e45ab913b86d7119
Jeannoel <sonsangjano@gmail.com>
2023-09-27 06:25:35 -0700

    ticket number

--- lightsail.tf
+++ lightsail.tf
@@ -1,8 +1,13 @@
-resource "aws_lightsail_instance" "gitlab-lightsail" {
+provider "aws" {
+  region = "us-east-1" 
+}
+
+resource "aws_lightsail_instance" "example" {
   name              = "lightsail-example"
   availability_zone = "us-east-1a" 
   blueprint_id      = "amazon_linux_2"
-  bundle_id         = "nano_2_0"
+  bundle_id         = "micro_2_0"
+
   user_data = <<-EOF
               #!/bin/bash
               sudo yum install httpd -y
